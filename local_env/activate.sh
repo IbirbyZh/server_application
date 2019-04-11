1608c0b3bf5bbbcd44ac23adcaddbb214a66f238
Zhenya Shishkin <040796@gmail.com>
2019-04-11 17:19:18 +0300

    add environ

---
+++ local_env/activate.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export GITLAB_PASSWORD=password
+export GITLAB_RUNNER_TOKEN=token
+export GITLAB_API_TOKEN=admin_token
+export DOCKER_DATA_PATH="/Users/zhshishkin/study/mipt/server/docker-data"
